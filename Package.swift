46d1d50e9742b2b7c5efe938d960a0eff0e34f6e
Vlastimir <vlastimir.radojevic@proglove.de>
2024-03-15 17:06:36 +0100

    adding release 1.0.0

--- Package.swift
+++ Package.swift
@@ -1,30 +1,35 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.7
 import PackageDescription
 
 let package = Package(
     name: "ConnectSDK",
     platforms: [
-        .watchOS(.v8)
+        .watchOS(.v9)
     ],
     products: [
         .library(
             name: "ConnectSDKWatch",
-            targets: ["ConnectSDKWatch", "Protobuf"])
+            targets: ["ConnectSDKWatch", "ConnectSDKWatchDependencies"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.16.0")
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.16.0"),
+        .package(url: "https://github.com/NordicSemiconductor/IOS-nRF-Connect-Device-Manager", from: "1.6.0"),
+        .package(url: "https://github.com/dagronf/QRCode", from: "17.0.0")
     ],
     targets: [
         .binaryTarget(
             name: "ConnectSDKWatch",
-            url: "https://dl.cloudsmith.io/CujHIwqxWVjq8tLK/proglove/markconnectiossdk-dev/raw/versions/1.8.0/ConnectSDKWatch-1.8.0.xcframework.zip",
-            checksum: "688267076362711b0cf17242928c60325af59a64388d2531c6177cf45bf368e6"),
+            url: "https://dl.cloudsmith.io/QQ43WPa2Y7VlFUM3/proglove/markconnectiossdk-prod/raw/names/ConnectSDKWatch-1.0.0.xcframework/versions/1.0.0/ConnectSDKWatch-1.0.0.xcframework.zip?accept_eula=5",
+            checksum: "f502698f572ba559301d7b1c217f66394ad86e93aaba29dc6e88b00261b3ccd8"),
         .target(
-            name: "Protobuf",
+            name: "ConnectSDKWatchDependencies",
             dependencies: [
-                .product(name: "SwiftProtobuf", package: "swift-protobuf")
+                "QRCode",
+                .target(name: "ConnectSDKWatch"),
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+                .product(name: "iOSMcuManagerLibrary", package: "IOS-nRF-Connect-Device-Manager")
             ],
-            path: "Sources/ConnectSDK"
+            path: "Sources/ConnectSDKWatch"
         )
     ]
 )
