c791d304cb31453161a964d825f5cb16ec4ff1ac
Dusan Brkic <dusan.brkic@proglove.de>
2024-06-13 14:38:03 +0200

    Release 1.1.0

--- Package.swift
+++ Package.swift
@@ -12,15 +12,15 @@ let package = Package(
             targets: ["ConnectSDKWatch", "ConnectSDKWatchDependencies"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.16.0"),
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.26.0"),
         .package(url: "https://github.com/NordicSemiconductor/IOS-nRF-Connect-Device-Manager", from: "1.6.0"),
-        .package(url: "https://github.com/dagronf/QRCode", from: "17.0.0")
+        .package(url: "https://github.com/dagronf/QRCode", from: "19.0.0")
     ],
     targets: [
         .binaryTarget(
             name: "ConnectSDKWatch",
-            url: "https://dl.cloudsmith.io/QQ43WPa2Y7VlFUM3/proglove/markconnectiossdk-prod/raw/names/ConnectSDKWatch-1.0.0.xcframework/versions/1.0.0/ConnectSDKWatch-1.0.0.xcframework.zip?accept_eula=5",
-            checksum: "f502698f572ba559301d7b1c217f66394ad86e93aaba29dc6e88b00261b3ccd8"),
+            url: "https://dl.cloudsmith.io/QQ43WPa2Y7VlFUM3/proglove/markconnectiossdk-prod/raw/names/ConnectSDKWatch-1.1.0.xcframework/versions/1.1.0/ConnectSDKWatch-1.1.0.xcframework.zip?accept_eula=7",
+            checksum: "f33ce56738abd181e92a0b24fafe8171aac3d914a38f37ece72d69b6e616648c"),
         .target(
             name: "ConnectSDKWatchDependencies",
             dependencies: [
